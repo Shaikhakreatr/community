fa42891201b64d88fb07901b716b6a1e84bfb8c3
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-06 20:46:38 +0530

    Hero class name space removed

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   
   return (
     <section>
-      <div className="container mx-auto ">
+      <div className="container mx-auto">
         <div className="grid md:grid-cols-2 sm:grid-cols-2 grid-cols-1 lg:py-24 sm:py-12 md:py-12 ml-3 py-14">
           <LeftSection />
           <RightSection />
