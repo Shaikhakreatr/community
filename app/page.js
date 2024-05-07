70bd7dc5b4ce3e6ce067d5a703c2de2a7bcfa507
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-07 20:03:54 +0530

    lg screen min-height set for wrapper

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Footer from './components/footer/Footer';
 const App = () => {
   return (
     <>
-      <div className='bg-img md:min-h-[100vh] lg:min-h-[auto]'>
+      <div className='bg-img md:min-h-[100vh] xl:min-h-[auto]'>
           <Header />  
           <Hero />
           <Footer />
