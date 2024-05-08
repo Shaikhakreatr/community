ebc2ca00d23de672878ce73a2ea2478c27f8d34f
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-08 11:28:42 +0530

    add new img for contact page

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Footer from './components/footer/Footer';
 const App = () => {
   return (
     <>
-      <main className='bg-img md:min-h-[100vh] xl:min-h-[auto]'>
+      <main className='bg-img md:min-h-[100vh] xl:min-h-[auto] flex flex-col overflow-x-hidden md:justify-between'>
           <Header />  
           <Hero />
           <Footer />
