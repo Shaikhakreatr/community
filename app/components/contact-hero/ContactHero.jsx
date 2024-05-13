8d0dbf2e3ce24f3d76134673ceec5b4f328611da
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-13 15:41:12 +0530

    testing

--- app/components/contact-hero/ContactHero.jsx
+++ app/components/contact-hero/ContactHero.jsx
@@ -7,7 +7,7 @@ const ContactHero = () => {
     <section>
       <div className="container mx-auto ">
         <div className="xl:py-[6.25rem] lg:mx-0 sm:mx-4 lg:py-[60px] py-[50px]">
-          <div className="sm:grid sm:grid-cols-5 xl:gap-24 sm:gap-20 gap-10 flex flex-col items-center justify-center">
+          <div className="sm:grid bg-orange-300 sm:grid-cols-5 xl:gap-24 sm:gap-20 gap-10 flex flex-col items-center justify-center">
             <LeftImageSection />
             <FormSection />
           </div>
