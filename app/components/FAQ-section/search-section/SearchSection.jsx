53f9dfc6642d119057e2868c9e558031fcb460bf
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-13 20:21:31 +0530

    classes updated in search section

--- app/components/FAQ-section/search-section/SearchSection.jsx
+++ app/components/FAQ-section/search-section/SearchSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchSection = () => {
   return (
     <div>
-      <div className="flex flex-col items-center sm:mb-0 mb-[50px] justify-center lg:mt-[3.5rem] lg:mb-[6.5rem] xl:mt-[4rem] xl:mb-[7rem]">
+      <div className="flex flex-col items-center sm:mb-0 mb-[50px] justify-center lg:mt-[3.5rem] lg:mb-[3.5rem] xl:mt-[4rem] xl:mb-[7rem]">
         <h1 className="page-heading leading-4 sm:mt-[45px] sm:mb-[45px] mt-[45px] relative text-center sm:text-[3.75rem] lg:text-[70px] xl:text-[96px] text-[36px]">
           Frequently Asked<span className="sm:hidden block"><br /></span> QuestIons
           <span className="absolute xl:top-[40%] xl:right-[-43%] lg:top-[168%] lg:right-[-12%] sm:top-[150%] sm:right-[-10%] right-[22%] bottom-[-90%]">
