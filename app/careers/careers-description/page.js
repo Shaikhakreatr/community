8d50503394dd1eea1711821697f5c37a210b0633
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-14 12:17:02 +0530

    created careers description page

---
+++ app/careers/careers-description/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CareersDescription = () => {
+  return (
+    <div>
+        <h1>careers description</h1>
+    </div>
+  )
+}
+
+export default CareersDescription
