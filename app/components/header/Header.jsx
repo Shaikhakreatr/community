7b62a8d053e38adeeea2016758384169d9303646
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-18 11:25:41 +0530

    added link to header logo

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import KreatrLogo from '../../../public/assets/images/home_page/logo/Kreatr-logo.svg'
 
 
@@ -9,7 +10,7 @@ const Header = () => {
     <>
       <header className="">
         <div className="flex items-center justify-center border-b border-gray-500 p-2">
-          <a href="/"><Image className="my-3 xl:h-32.79 xl:w-181 lg:w-[10rem] lg:h-[1.6875rem] w-124 h-22"  src={KreatrLogo} alt="Kreatr Logo" /></a>
+          <Link href="/"><Image className="my-3 xl:h-[32px] xl:w-[181px] lg:w-[10rem] lg:h-[1.6875rem] w-124 h-22"  src={KreatrLogo} alt="Kreatr Logo" /></Link>
         </div>
       </header>
     </>
