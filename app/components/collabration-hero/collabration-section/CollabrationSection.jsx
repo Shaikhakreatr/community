418def5f5515c63d849f22dc82a99ca4d8b44ec3
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-18 12:12:08 +0530

    removed apos

--- app/components/collabration-hero/collabration-section/CollabrationSection.jsx
+++ app/components/collabration-hero/collabration-section/CollabrationSection.jsx
@@ -12,7 +12,7 @@ const CollabrationSection = () => {
     <div>
       <div>
         <Tabs variant="pills" radius="lg" defaultValue="speakers">
-          <div className="sm:mt-[80px] mt-[50px] flex flex-col items-center justify-center">
+          <div className="mt-[50px] flex flex-col items-center justify-center sm:mt-[80px]">
             <div className="flex justify-center">
               <Tabs.List>
                 <Tabs.Tab value="speakers">Speakers</Tabs.Tab>
@@ -23,19 +23,20 @@ const CollabrationSection = () => {
             </div>
             {/* speakers section */}
             <Tabs.Panel value="speakers">
-              <div className="sm:mt-[50px] mt-[35px]">
+              <div className="mt-[35px] sm:mt-[50px]">
                 <div>
                   <h1 className="page-subhead partner-text text-center text-[20px] sm:text-[28px] lg:text-[32px] xl:text-[34px]">
-                    Do you love spreading knowledge<br className="sm:hidden" /> and igniting minds as a
+                    Do you love spreading knowledge
+                    <br className="sm:hidden" /> and igniting minds as a
                     speaker?
                   </h1>
                   <p className="content-neue-medium text-center text-[14px] italic sm:text-[22px] lg:text-[26px] xl:text-[28px] ">
-                    Here&#39;s what happens when you collaborate with us!
+                    Heres what happens when you collaborate with us!
                   </p>
                 </div>
               </div>
-              <div className="sm:mx-[40px] sm:my-[100px] sm:flex items-start justify-center">
-                <div className="sm:border-r border-black px-[20px] sm:px-[50px] sm:w-[50%]">
+              <div className="items-start justify-center sm:mx-[40px] sm:my-[100px] sm:flex">
+                <div className="border-black px-[20px] sm:w-[50%] sm:border-r sm:px-[50px]">
                   {speakers.map((item, index) => (
                     <div key={index}>
                       <ul className="page-subhead text-[18px] sm:text-[22px] lg:text-[26px]  xl:text-[28px]">
@@ -43,7 +44,7 @@ const CollabrationSection = () => {
                           {item.title}
                         </li>
                       </ul>
-                      <p className="content-neue-medium text-[14px] lg:leading-[28px] leading-[17px] sm:text-[20px] lg:text-[24px] xl:text-[26px]">
+                      <p className="content-neue-medium text-[14px] leading-[17px] sm:text-[20px] lg:text-[24px] lg:leading-[28px] xl:text-[26px]">
                         {item.description}
                       </p>
                     </div>
@@ -56,18 +57,19 @@ const CollabrationSection = () => {
             </Tabs.Panel>
             {/* sponsors section */}
             <Tabs.Panel value="sponsors">
-              <div className="sm:mt-[50px] mt-[35px]">
+              <div className="mt-[35px] sm:mt-[50px]">
                 <div>
                   <h1 className="page-subhead partner-text text-center text-[20px] sm:text-[28px] lg:text-[32px] xl:text-[34px]">
-                    Turbocharge your brand&#39;s<br className="sm:hidden" /> visibility through sponsorship!
+                    Turbocharge your brands
+                    <br className="sm:hidden" /> visibility through sponsorship!
                   </h1>
                   <p className="content-neue-medium text-center text-[14px] italic sm:text-[22px] lg:text-[26px] xl:text-[28px] ">
-                    Here&#39;s what happens when you collaborate with us!
+                    Heres what happens when you collaborate with us!
                   </p>
                 </div>
               </div>
-              <div className="sm:mx-[40px] sm:my-[100px] sm:flex items-start justify-center">
-                <div className="sm:border-r border-black px-[20px]  sm:px-[50px] sm:w-[50%]">
+              <div className="items-start justify-center sm:mx-[40px] sm:my-[100px] sm:flex">
+                <div className="border-black px-[20px] sm:w-[50%]  sm:border-r sm:px-[50px]">
                   {sponsors.map((item, index) => (
                     <div key={index}>
                       <ul className="page-subhead text-[18px] sm:text-[22px] lg:text-[26px]  xl:text-[28px]">
@@ -75,7 +77,7 @@ const CollabrationSection = () => {
                           {item.title}
                         </li>
                       </ul>
-                      <p className="content-neue-medium text-[14px] lg:leading-[28px] leading-[17px] sm:text-[20px] lg:text-[24px] xl:text-[26px]">
+                      <p className="content-neue-medium text-[14px] leading-[17px] sm:text-[20px] lg:text-[24px] lg:leading-[28px] xl:text-[26px]">
                         {item.description}
                       </p>
                     </div>
@@ -89,18 +91,18 @@ const CollabrationSection = () => {
 
             {/* spaces section */}
             <Tabs.Panel value="spaces">
-              <div className="sm:mt-[50px] mt-[35px]">
+              <div className="mt-[35px] sm:mt-[50px]">
                 <div>
                   <h1 className="page-subhead partner-text text-center text-[20px] sm:text-[28px] lg:text-[32px] xl:text-[34px]">
                     Got a great space to offer?
                   </h1>
                   <p className="content-neue-medium text-center text-[14px] italic sm:text-[22px] lg:text-[26px] xl:text-[28px] ">
-                    Here&#39;s what happens when you collaborate with us!
+                    Heres what happens when you collaborate with us!
                   </p>
                 </div>
               </div>
-              <div className="sm:mx-[40px] sm:my-[100px] sm:flex items-start justify-center">
-                <div className="sm:border-r border-black px-[20px]  sm:px-[50px] sm:w-[50%]">
+              <div className="items-start justify-center sm:mx-[40px] sm:my-[100px] sm:flex">
+                <div className="border-black px-[20px] sm:w-[50%]  sm:border-r sm:px-[50px]">
                   {spaces.map((item, index) => (
                     <div key={index}>
                       <ul className="page-subhead text-[18px] sm:text-[22px] lg:text-[26px]  xl:text-[28px]">
@@ -108,7 +110,7 @@ const CollabrationSection = () => {
                           {item.title}
                         </li>
                       </ul>
-                      <p className="content-neue-medium text-[14px] lg:leading-[28px] leading-[17px] sm:text-[20px] lg:text-[24px] xl:text-[26px]">
+                      <p className="content-neue-medium text-[14px] leading-[17px] sm:text-[20px] lg:text-[24px] lg:leading-[28px] xl:text-[26px]">
                         {item.description}
                       </p>
                     </div>
@@ -122,18 +124,19 @@ const CollabrationSection = () => {
 
             {/* sellers section */}
             <Tabs.Panel value="sellers">
-              <div className="sm:mt-[50px] mt-[35px]">
+              <div className="mt-[35px] sm:mt-[50px]">
                 <div>
                   <h1 className="page-subhead partner-text text-center text-[20px] sm:text-[28px] lg:text-[32px] xl:text-[34px]">
-                    Wish to sell your products<br className="sm:hidden" /> or services as a seller?
+                    Wish to sell your products
+                    <br className="sm:hidden" /> or services as a seller?
                   </h1>
                   <p className="content-neue-medium text-center text-[14px] italic sm:text-[22px] lg:text-[26px] xl:text-[28px] ">
-                    Here&#39;s what happens when you collaborate with us!
+                    Heres what happens when you collaborate with us!
                   </p>
                 </div>
               </div>
-              <div className="sm:mx-[40px] sm:my-[100px] sm:flex items-start justify-center">
-                <div className="sm:border-r border-black px-[20px]  sm:px-[50px] sm:w-[50%]">
+              <div className="items-start justify-center sm:mx-[40px] sm:my-[100px] sm:flex">
+                <div className="border-black px-[20px] sm:w-[50%]  sm:border-r sm:px-[50px]">
                   {sellers.map((item, index) => (
                     <div key={index}>
                       <ul className="page-subhead text-[18px] sm:text-[22px] lg:text-[26px]  xl:text-[28px]">
@@ -141,7 +144,7 @@ const CollabrationSection = () => {
                           {item.title}
                         </li>
                       </ul>
-                      <p className="content-neue-medium text-[14px] lg:leading-[28px] leading-[17px] sm:text-[20px] lg:text-[24px] xl:text-[26px]">
+                      <p className="content-neue-medium text-[14px] leading-[17px] sm:text-[20px] lg:text-[24px] lg:leading-[28px] xl:text-[26px]">
                         {item.description}
                       </p>
                     </div>
