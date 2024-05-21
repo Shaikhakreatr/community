63c35f42a5ed3747392952aa665dd78e3a68704a
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-21 17:31:43 +0530

    done page for about kreatr

--- app/components/about-kreatr/about-kreatr-header/AboutKreatrHeader.jsx
+++ app/components/about-kreatr/about-kreatr-header/AboutKreatrHeader.jsx
@@ -1,15 +1,60 @@
 import React from "react";
+import Image from "next/image";
+import ArrowImage from "../../../../public/assets/images/about-kreatr/about-kreatr-arrow.svg";
 
 const AboutKreatrHeader = () => {
   return (
     <div className="mt-[30px]">
-      <h1 className="page-heading text-center text-[48px] sm:text-[64px] lg:text-[86px] xl:text-[96px]">
-        About Kreatr
-      </h1>
-      <p className="content-neue text-center text-[16px] italic leading-[18px] sm:text-[18px] lg:text-[22px] lg:leading-[26px] xl:text-[24px]">
-        We are a digital ecosystem that encourages<br className="sm:hidden block" /> cross-industry<br className="sm:block hidden" /> collaboration
-        and empowers<br className="sm:hidden block" /> creators like you.
-      </p>
+      <div>
+        <h1 className="page-heading text-center text-[48px] sm:text-[74px] lg:text-[96px] xl:text-[110px]">
+          About Kreatr
+        </h1>
+        <p className="content-neue text-center text-[16px] italic leading-[18px] sm:text-[18px] lg:text-[22px] lg:leading-[26px] xl:text-[24px]">
+          We are a digital ecosystem that encourages
+          <br className="block sm:hidden" /> cross-industry
+          <br className="hidden sm:block" /> collaboration and empowers
+          <br className="block sm:hidden" /> creators like you.
+        </p>
+      </div>
+      <div className="mx-[20px] mt-[45px] flex items-start justify-center gap-[28px] sm:mx-[60px] sm:mt-[100px] sm:gap-[90px]">
+        <div className="items-center sm:flex">
+          <h1 className="page-heading mb-[16px] text-[32px] leading-[28px] sm:mb-0 sm:text-[48px] lg:text-[58px] lg:leading-[48px] xl:text-[64px] xl:leading-[52px]">
+            How DId
+            <br /> We Start?
+          </h1>
+          <Image
+            className="h-[44px] w-[44px] lg:h-[96px] lg:w-[99px]"
+            src={ArrowImage}
+            alt="arrow img"
+          />
+        </div>
+        <div className="content-neue w-[56%] text-[14px] leading-[18px] sm:w-[60%] lg:text-[26px] lg:leading-[32px] xl:text-[28px]">
+          <p>
+            {`It all started when we, Megha and Abdul—the co-founders of
+            Kreatr—sought a creative community that matched our vibe.`}
+          </p>
+          <br />
+          <p>{`Eager to
+            connect with like-minded individuals across diverse design fields,
+            we struggled to find a space that celebrated the joy of creation.`}</p>
+          <br />
+          <p>{`So, we took a leap of faith and launched Kreatr with a vision to
+            unite a klan of similar-minded creatives.`}</p>
+          <br />
+          <p>{` Our mission goes beyond
+            borders; we're committed to spotlighting Indian designers globally
+            and acknowledging their talent and contributions. `}</p>
+          <br />
+          <p>{`Through Kreatr,
+            we've built a digital ecosystem where creatives from all backgrounds
+            can learn, collaborate, and inspire each other.`}</p>
+        </div>
+      </div>
+      <div className="ml-[56px] mt-[20px] flex justify-center sm:ml-0 sm:mt-[50px] lg:mr-[130px]">
+        <div className="page-subhead kreatr-btn h-[32px] w-[114px] rounded-[40px] bg-black lg:pt-[14px] pt-[7px] text-center text-[12px] lg:h-[58px] lg:w-[251px] lg:text-[18px] xl:text-[24px]">
+          Join Our Initiative
+        </div>
+      </div>
     </div>
   );
 };
--- app/components/about-kreatr/about-kreatr-middle/AboutKreatrMiddle.jsx
+++ app/components/about-kreatr/about-kreatr-middle/AboutKreatrMiddle.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Connect from "../../../../public/assets/images/about-kreatr/connect-kreatr.svg";
+import Collaborate from "../../../../public/assets/images/about-kreatr/collaborate-kreatr.svg";
+import Create from "../../../../public/assets/images/about-kreatr/create-kreatr.svg";
+import AboutKreatrImg1 from "../../../../public/assets/images/about-kreatr/kreatr-img1.jpg";
+import AboutKreatrImg2 from "../../../../public/assets/images/about-kreatr/kreatr-img2.jpg";
+import Commas from "../../../../public/assets/images/about-kreatr/inverted-commas.svg";
 
 const AboutKreatrMiddle = () => {
   return (
-    <div>AboutKreatrMiddle</div>
-  )
-}
+    <div className="relative">
+      <div>
+        <p className="page-subhead mt-[30px] text-center text-[16px] sm:mt-[50px] lg:text-[28px] xl:text-[35px]">
+          We Are Guided By 3C’s
+        </p>
+      </div>
+      <div className="mt-[55px] flex items-center justify-center sm:mt-[50px] sm:mb-0 mb-[30px]">
+        <div className="relative flex w-[50%] items-center justify-center sm:h-0 h-[60px]">
+          <Image className="absolute top-[-50%] left-[-15%] xl:left-[-24%] lg:left-[-17%] h-[49px] w-[122px] lg:h-[125px] lg:w-[340px] xl:h-[155px] xl:w-[389px] rotate-[-2deg]" src={Connect} alt="connect image" />
+          <Image className="absolute top-[-45%] right-[-25%] xl:top-[10px] xl:right-[8%] lg:top-[10px] lg:right-[12%] h-[42px] w-[145px] lg:h-[110px] lg:w-[400px] xl:h-[135px] xl:w-[461px] rotate-[2deg]" src={Collaborate} alt="collaborate image" />
+          <Image className="absolute top-[25%] h-[45px] w-[103px] lg:h-[121px] lg:w-[320px] lg:left-[82%] xl:left-[90%] xl:h-[145px] xl:w-[328px] rotate-[-2deg]" src={Create} alt="create image" />
+        </div>
+      </div>
+      <div className="mt-[30px] flex justify-center sm:mt-[190px]">
+        <p className="content-neue-medium text-center lg:leading-[30px] xl:leading-[32px] leading-[18px] text-[14px] lg:text-[26px] xl:text-[28px]">
+          Our mission is to build a sustainable community of<br className="sm:hidden" /> creatives
+          <br className="sm:block hidden" /> who aspire to change the world through<br className="sm:hidden" /> their designs and
+          <br className="sm:block hidden" /> empower fellow creatives from<br className="sm:hidden" /> all backgrounds to do the same.
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-center">
+          <div className="relative mt-[190px] grid-cols-2 sm:mt-[50px] sm:grid sm:gap-[240px] lg:gap-[90px] xl:gap-[0px]">
+            <div className="kreatr-box1 h-[323px] w-[392px] pt-[150px] px-[52px]  sm:p-[85px] lg:h-[340px] lg:w-[650px] lg:p-[78px] xl:h-[364px] xl:w-[669px]">
+              <h1 className="page-heading text-center text-[40px]  leading-[35px] sm:text-start sm:text-[60px] lg:text-[70px] lg:leading-[56px] xl:text-[80px] xl:leading-[67px]">
+                DIRECTION
+              </h1>
+              <p className="content-neue mt-[15px] text-center text-[16px] leading-[18px] text-black sm:mt-[28px] sm:text-start sm:text-[20px]  sm:leading-[23px] lg:text-[22px] lg:leading-[32px] xl:text-[24px]">
+                To ignite a revolution in the design industry through
+                cross-industry collaborations that raise the bar and to
+                establish a global standard for creativity.
+              </p>
+            </div>
+            <div className="absolute left-[11%] top-[-46%] sm:static">
+              <Image
+                className="h-[271px] w-[320px] rounded-[15px] lg:h-[340px] lg:w-[550px] xl:h-[364px] xl:w-[650px]"
+                src={AboutKreatrImg1}
+                alt="Kreatr image"
+              />
+            </div>
+          </div>
+          <div className="relative mt-[195px] justify-end sm:mt-[50px] sm:flex xl:gap-[50px] lg:gap-[50px]">
+            <div className="absolute left-[11%] top-[-46%] sm:static">
+              <Image
+                className="h-[271px] w-[320px] rounded-[15px] lg:h-[340px] lg:w-[650px] xl:h-[364px] xl:w-[727px]"
+                src={AboutKreatrImg2}
+                alt="Kreatr image"
+              />
+            </div>
+            <div className="kreatr-box2 h-[323px] w-[392px] px-[52px] pt-[150px] sm:p-[96px] lg:h-[340px] lg:w-[520px] lg:p-[72px] xl:h-[364px] xl:w-[690px]">
+              <h1 className="page-heading text-center text-[40px]  leading-[35px] sm:text-start sm:text-[60px] lg:text-[70px] lg:leading-[56px] xl:text-[80px] xl:leading-[67px]">
+                DESTINY
+              </h1>
+              <p className="content-neue mt-[24px] list-disc text-center text-[16px] leading-[18px] text-black sm:mt-[28px] sm:text-start sm:text-[20px] lg:text-[22px] lg:leading-[24px] xl:text-[24px] xl:leading-[32px]">
+                To create a unique digital ecosystem for high-spirited creatives
+                where they can unleash their imagination, grow their skills, and
+                share their talents with other creatives.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="sm:px-[100px] sm:py-[50px] sm:mt-0 mt-[40px]">
+        <h1 className="page-heading text-center xl:text-[96px] lg:text-[86px] text-[40px]">Creators</h1>
+        <div className="lg:mt-[40px] mt-[40px] flex items-start justify-between px-[18px] sm:px-0 lg:gap-[30px]">
+          <div>
+            <p className="page-subhead xl:text-[30px] lg:text-[26px] text-[14px] xl:leading-[32px] lg:leading-[30px] leading-[18px] italic">
+              “As designers, we have been hiding<br className="sm:hidden" /> our true
+              <br className="sm:block hidden" /> potential for too long. It is now<br className="sm:hidden" /> time to be that change.”
+            </p>
+            <p className="page-subhead lg:mt-[20px] mt-[10px] xl:text-[30px] lg:text-[26px] text-[14px] xl:leading-[32px] lg:leading-[30px] leading-[18px]">
+              – Abdul Rahiman
+              <br /> Co-founder & CEO
+            </p>
+          </div>
+          <div className="sm:mr-[80px]">
+            <Image className="lg:h-[80px] lg:w-[80px] h-[40px] w-[60px]" src={Commas} alt="inverted commas" />
+          </div>
+        </div>
+        <div className="flex justify-end sm:mt-0 mt-[20px]">
+          <div className="sm:w-[56%] w-[64%]">
+            <p className="page-subhead xl:text-[30px] lg:text-[26px] text-[14px] xl:leading-[32px] lg:leading-[30px] leading-[18px] italic">
+              {`“We're a bunch of crazy people who want to show you”`}
+            </p>
+            <p className="page-subhead xl:text-[30px] lg:text-[26px] text-[14px] xl:leading-[32px] lg:leading-[30px] leading-[18px] italic">{`what we can do.
+            Welcome to the community!`}</p>
+            <p className="page-subhead lg:mt-[20px] mt-[10px] xl:text-[30px] lg:text-[26px] text-[14px] xl:leading-[32px] lg:leading-[30px] leading-[18px]">
+              – Megha Satish
+              <br /> Co-founder & Creative Director
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutKreatrMiddle
+export default AboutKreatrMiddle;
