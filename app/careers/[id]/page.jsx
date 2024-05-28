9f126e414579ef17933dc830691529af9c388c7a
Shaikhakreatr <shaikha@thekreatr.com>
2024-05-28 16:12:55 +0530

    changes in careers main content

--- app/careers/[id]/page.jsx
+++ app/careers/[id]/page.jsx
@@ -1,12 +1,47 @@
-import { useRouter } from 'next/router';
-import CareersMainContent from '@/components/careers-description/CareersMainContent';
-import Header from '@/components/header/Header';
-import Footer from '@/components/footer/Footer';
+'use client';
 
-const CareerDetails = ({ careerData, error }) => {
-  const router = useRouter();
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import Footer from '@/app/components/footer/Footer';
+import Header from '@/app/components/header/Header';
+import React from 'react';
+import CareersMainContent from '@/app/components/careers-description/CareersMainContent';
 
-  if (router.isFallback) {
+const CareerDetails = () => {
+  const { id } = useParams();
+
+  const [careerData, setCareerData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCareerData = async () => {
+      try {
+        const response = await fetch('https://pcfja54uwi.execute-api.ap-south-1.amazonaws.com/dev/career/');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const careerData = data.find(career => career.id === id);
+        if (careerData) {
+          setCareerData(careerData);
+        } else {
+          setError(new Error('Career not found'));
+        }
+      } catch (error) {
+        console.error('Error fetching career data:', error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (id) {
+      fetchCareerData();
+    }
+  }, [id]);
+
+  if (loading) {
     return <div>Loading...</div>;
   }
 
@@ -18,34 +53,14 @@ const CareerDetails = ({ careerData, error }) => {
     return <div>Career not found</div>;
   }
 
+  console.log(careerData);
   return (
     <main className='pt-[6.25rem] bg-img'>
       <Header />
-      <CareersMainContent careerData={careerData} />
+      <CareersMainContent careerData={careerData}/>
       <Footer />
     </main>
   );
 };
-
-export async function getServerSideProps(context) {
-  const { id } = context.params;
-
-  try {
-    const response = await fetch('https://pcfja54uwi.execute-api.ap-south-1.amazonaws.com/dev/career/');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    const careerData = data.find(career => career.id === id);
-
-    if (!careerData) {
-      return { props: { error: { message: 'Career not found' } } };
-    }
-
-    return { props: { careerData } };
-  } catch (error) {
-    return { props: { error: { message: error.message } } };
-  }
-}
 
 export default CareerDetails;
