768da8ea9ee9cfded9780f066a10ab8430a4a4bb
Shaikhakreatr <shaikha@thekreatr.com>
2024-07-03 10:56:22 +0530

    changes in events listing

--- app/components/events-listing/events-hero/EventsHero.jsx
+++ app/components/events-listing/events-hero/EventsHero.jsx
@@ -120,7 +120,7 @@ const EventsHero = () => {
                           }}
                         >
                           <img
-                            className="h-[225.63px] w-[261.18px] rounded-[15px] object-cover object-center lg:h-[250px] lg:w-[280px] xl:h-[319.77px] xl:w-[348px]"
+                            className="h-[225.63px] w-[261.18px] rounded-[15px] object-cover object-left lg:h-[250px] lg:w-[280px] xl:h-[319.77px] xl:w-[348px]"
                             src={event.coverImg}
                             alt="upcoming img"
                           />
@@ -158,7 +158,7 @@ const EventsHero = () => {
                         }}
                       >
                         <img
-                          className="h-[225.63px] w-[261.18px] rounded-[15px] object-cover object-center lg:h-[250px] lg:w-[280px] xl:h-[319.77px] xl:w-[348px]"
+                          className="h-[225.63px] w-[261.18px] rounded-[15px] object-cover object-left lg:h-[250px] lg:w-[280px] xl:h-[319.77px] xl:w-[348px]"
                           src={event.coverImg}
                           alt="past img"
                         />
