39d6c897268a1137142312cc42e53e5dcf549c41
Shaikhakreatr <shaikha@thekreatr.com>
2024-07-09 15:36:53 +0530

    done changes in payment confirmation

--- app/components/payment-failure/PaymentFailureMain.jsx
+++ app/components/payment-failure/PaymentFailureMain.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const PaymentFailureMain = () => {
   return (
-    <div className='h-full lg:mt-[60px] mt-[50px] w-full flex flex-col justify-center items-center'>
+    <div className='h-full lg:mt-[60px] sm:mt-[50px] mt-[80px] mb-[50px] sm:mb-0 w-full flex flex-col justify-center items-center'>
         <Image
         src={"/assets/images/payment/failure-img.svg"}
         alt='image'
@@ -13,8 +13,8 @@ const PaymentFailureMain = () => {
         className='flex flex-col justify-center items-center xl:h-[130px] xl:w-[124px] lg:h-[110px] lg:w-[110px] sm:h-[80px] sm:w-[80px] h-[52px] w-[52px] '/>
         <h1 className='page-subhead text-[#E41E4F] font-[800] xl:text-[40px] lg:text-[32px] sm:text-[26px] text-[20px] lg:mt-14 sm:mt-[30px] mt-[25px] xl:leading-[40px] lg:leading-[38px]'>Oops! Something went wrong.</h1>
         <h1 className={'content-neue-medium xl:mt-[10px] lg:mt-[8px] sm:mt-[5px] mt-[5px]  xl:text-[28px] lg:text-[20px] text-[#2A2A2A] sm:text-[18px] text-[14px] text-center'}>{`Let’s give it another shot!`}</h1>
-        <div className='bg-[#2A2A2A] cursor-pointer hover:bg-transparent hover:border hover:border-[#2A2A2A] hover:text-[#2A2A2A] xl:text-[25px] lg:text-[20px] sm:text-[16px] text-[12px] flex justify-center items-center sm:mt-[50px] mt-[30px] sm:mb-[80px] mb-[50px] rounded-[30px] text-[#FFFDEE] xl:w-[265px] xl:h-[67px] lg:h-[50px] lg:w-[200px] sm:h-[40px] sm:w-[170px] h-[30px] w-[114px]'>
-            PROCEED TO PAY
+        <div className='bg-[#2A2A2A] cursor-pointer sm:hover:bg-transparent sm:hover:border sm:hover:border-[#2A2A2A] sm:hover:text-[#2A2A2A] xl:text-[20px] lg:text-[16px] sm:text-[16px] text-[12px] flex justify-center items-center sm:mt-[50px] mt-[30px] sm:mb-[80px] mb-[50px] rounded-[30px] text-[#FFFDEE] xl:w-[265px] xl:h-[67px] lg:h-[50px] lg:w-[200px] sm:h-[40px] sm:w-[170px] h-[30px] w-[114px]'>
+        Proceed to Pay
         </div>
     </div>
   )
--- app/components/payment-success/PaymentSuccessMain.jsx
+++ app/components/payment-success/PaymentSuccessMain.jsx
@@ -1,79 +1,57 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import useIsMobile from "@/app/hooks/customHook";
-import PaymentSuccessMob from "./payment-success-mob/PaymentSuccessMob";
 
 const PaymentSuccessMain = () => {
-  const isMobile = useIsMobile();
   return (
-    <div
-      className={
-        isMobile
-          ? "w-full h-[94vh] mt-[30px] flex flex-col items-center"
-          : "w-full h-[94vh] mt-[50px]  flex flex-col justify-center items-center"
-      }
-    >
-      {isMobile ? (
-        <PaymentSuccessMob />
-      ) : (
-        <>
-        
+    <div className="container mx-auto">
+      <div className="mt-[40px] flex flex-col items-center justify-center sm:mt-[44px] lg:mt-[60px] xl:mt-[72px]">
+        <div className="flex flex-col items-center justify-center">
           <Image
-            src={"/assets/images/payment/tickmark_successful.svg"}
-            alt="success"
-            height={123.03}
-            width={129.04}
-            className="mt-[50 px] xl:h-[122px] xl:w-[130px] lg:h-[70px] lg:w-[70px] sm:h-[90px] sm:w-[95px]"
+            src="/assets/images/payment/green-tick.svg"
+            className="h-[46px] w-[49px] sm:h-[102px] sm:w-[98px]"
+            height={102}
+            width={98}
           />
-
-          <h1 className="page-subhead mb-[20px] mt-[5px] xl:text-[35px] lg:text-[26px] sm:text-[24px]">
-            {`You're good to go!`}
-          </h1>
-          <div
-            className="xl:h-[520px] lg:mb-[80px] xl:w-[700px] lg:h-[365px] lg:w-[500px] sm:h-[400px] sm:w-[700px] lg:px-[25px] lg:py-[15px]"
-            style={{
-              backgroundImage: "url('/assets/images/payment/payment-card.png')",
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            <div className="gap-[15px] flex">
-            <Image src="/assets/images/payment/payment-success.png"  height={334} width={334} alt="payment success" />
-              <div className="flex flex-col justify-center ">
-                <h1 className=" page-subhead xl:text-[35px] leading-[30px] mb-[18px] lg:text-[24px] sm:text-[22px]">
-                  Design Thinking Workshop-Extended Version
-                </h1>
-                <p className="xl:text-[30px] lg:text-[20px] text-start sm:text-[16px]">Sat 12 Jan 2024</p>
-                <p className="xl:text-[30px] lg:text-[20px] text-start sm:text-[16px]">10:00 AM</p>
-                <p className="xl:text-[30px] lg:text-[20px] text-start sm:text-[16px]">Slay Coffee, Koramangala</p>
-              </div>
+          <h2 className="page-subhead text-[16px] text-[#49A549] sm:text-[22px] lg:text-[32px] xl:text-[40px]">
+            Ticket payment successful
+          </h2>
+        </div>
+        <div className="mt-[24px] flex flex-col items-center justify-center sm:mt-[28px] lg:mt-[32px] xl:mt-[44px] mb-[30px] sm:mb-[34px] lg:mb-[46px] xl:mb-[52px] ">
+          <Image
+            src="/assets/images/payment/ticket img.svg"
+            height={122}
+            width={100}
+            className="h-[310px] w-[280px] sm:h-[400px] sm:w-[380px] lg:h-[560px] lg:w-[530px] xl:h-[692px] xl:w-[612px]"
+          />
+          <div className="absolute">
+            <h2 className="page-subhead text-center text-[16px] leading-[17px] text-[#2A2A2A] sm:text-[22px] sm:leading-[32px] lg:text-[32px] lg:leading-[36px] xl:text-[40px] xl:leading-[40px]">
+              Design Thinking Workshop-
+              <br />
+              Extended Version
+            </h2>
+            <div className="content-neue-medium mt-[20px] flex flex-col items-center justify-center gap-[8px] text-[12px] text-[#2A2A2A] sm:mt-[24px] sm:text-[16px] lg:text-[20px] xl:text-[30px]">
+              <h3>Sat 12 Jan, 2024</h3>
+              <h3>10:00 AM</h3>
+              <h3>Slay Coffee, Koramangala</h3>
             </div>
-            <div className="flex flex-col justify-between ">
-              <div className=" flex justify-center mt-[40px]">
-                <h1 className="text-[1.5vw] content font-[800]">
-                  Booking ID:89DJNSOOSDW
-                </h1>
-              </div>
-              <Image
-                src={"/assets/images/payment/divider.svg"}
-                alt="divider"
-                height={0}
-                width={700}
-                className="mt-[21px] mb-[10px]"
-              />
-              <div className="flex justify-between p-[2%]">
-                <h1>Total Amount</h1>
-                <h1>INR 499.00</h1>
-              </div>
+            <h4 className="content-neue mt-[20px] text-center text-[14px] text-[rgba(42,42,42,0.4)] sm:text-[18px] lg:text-[20px] xl:text-[24px]">
+              The ticket is sent to aava@thekreatr.com
+            </h4>
+            <h2 className="page-subhead mt-[24px] text-center text-[16px] leading-[17px] text-[#2A2A2A] sm:text-[22px] sm:leading-[32px] lg:text-[32px] lg:leading-[36px] xl:text-[40px] xl:leading-[40px]">
+              Booking ID : 89DJNS00SDW
+            </h2>
+            <div className="flex justify-center items-between">
+            <div className="content-neue-medium mt-[24px] flex items-center justify-center gap-[100px] lg:gap-[200px] xl:gap-[260px] text-[10px] sm:text-[18px] lg:text-[20px] xl:text-[24px]">
+              <h4>Total Amount</h4>
+              <h4>INR 1499.00</h4>
+            </div>
             </div>
           </div>
-        </>
-      )}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default PaymentSuccessMain;
-
