e2ec128cfa21e9a73b423c7cb416466a22fa8852
Shaikhakreatr <shaikha@thekreatr.com>
2024-07-20 12:52:41 +0530

    initial git add

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -7,7 +7,7 @@ import Spinner from '@/app/components/spinner/Spinner';
 
 const BlogDetailsHero = () => {
   const { id } = useParams();
-  const [htmlData, setHtmlData] = useState(null); // Initialize with null to check if data is loaded
+  
   const [loading, setLoading] = useState(true); // Initialize loading state
 
   useEffect(() => {
@@ -15,7 +15,6 @@ const BlogDetailsHero = () => {
       try {
         const response = await fetch(`https://mik4tx7ct3.execute-api.ap-south-1.amazonaws.com/dev/blogs/${id}`);
         const data = await response.json();
-        setHtmlData(data);
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -32,7 +31,9 @@ const BlogDetailsHero = () => {
       {loading ? (
         <Spinner loading={loading} />
       ) : (
-        <div className='w-full h-full flex flex-col justify-center items-center xl:mt-[140px] lg:mt-[80px] sm:mt-[60px] mt-[44px]' dangerouslySetInnerHTML={{ __html: htmlData?.htmlPage || '<p>No content available</p>' }} />
+        <div className='w-full h-full flex flex-col justify-center items-center xl:mt-[140px] lg:mt-[80px] sm:mt-[60px] mt-[44px]'>
+          hello world
+          </div>
       )}
       <Footer />
     </main>
